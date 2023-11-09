7c4ff5c02f7f396681bda6f449eb49aa1436f4b8
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 22:29:19 -0500

    Update eight_bit_adder.py

--- my_package/logic_gates/eight_bit_adder.py
+++ my_package/logic_gates/eight_bit_adder.py
@@ -1,4 +1,7 @@
 def eight_bit_adder(a, b):
+    if len(a) != 8 or len(b) != 8:
+        raise ValueError("Input numbers must be eight bits long")
+
     total = [0] * 8  # Initialize the result as an eight-element list of zeros
     carry = 0  # Initialize the carry as 0
 
@@ -6,4 +9,4 @@ def eight_bit_adder(a, b):
     for i in range(7, -1, -1):
         carry, total[i] = adder(int(a[i]), int(b[i]), carry)
 
-    return carry, total
+    return total
