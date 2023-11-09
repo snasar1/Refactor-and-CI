37a45002852732418bcdea201c5dafc91ad1e008
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 19:36:18 -0500

    Update or_gate.py

--- my_package/logic_gates/or_gate.py
+++ my_package/logic_gates/or_gate.py
@@ -1,1 +1,9 @@
+# Define a class for the OR gate
+class ORGate:
+    def __init__(self, label):
+        self.label = label
 
+    # Implement the OR gate logic
+    def perform_gate_logic(self, input_a, input_b):
+        # The OR gate returns True if at least one input is True, otherwise False
+        return input_a or input_b
