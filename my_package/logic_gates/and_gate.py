bfeaf85dc3e387468d429b830a02aa16f86bbc40
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 19:35:38 -0500

    Update and_gate.py

--- my_package/logic_gates/and_gate.py
+++ my_package/logic_gates/and_gate.py
@@ -1,1 +1,9 @@
+# Define a class for the AND gate
+class ANDGate:
+    def __init__(self, label):
+        self.label = label
 
+    # Implement the AND gate logic
+    def perform_gate_logic(self, input_a, input_b):
+        # The AND gate returns True if both inputs are True, otherwise False
+        return input_a and input_b
