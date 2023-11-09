23880a332b448d38424d5435085a3acaa621a5bc
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 21:00:27 -0500

    Create setup.py

---
+++ my_package/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="my_package",
+    version="0.1",
+    author="Sumira Nasar",
+    description="MyPackage is a Python package designed to provide a collection of logic gates and connectors for building digital circuits. It simplifies the implementation of logic gates such as AND, OR, and NOT gates, as well as the connections between them.",
+    packages=find_packages(),
+)
+
+
+
+
+
