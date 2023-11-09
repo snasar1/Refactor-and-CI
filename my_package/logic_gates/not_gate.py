55fd3b15e7847dac5c592a8d534f1c89872d4ce3
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 19:36:41 -0500

    Update not_gate.py

--- my_package/logic_gates/not_gate.py
+++ my_package/logic_gates/not_gate.py
@@ -1,1 +1,9 @@
+# Define a class for the NOT gate
+class NOTGate:
+    def __init__(self, label):
+        self.label = label
 
+    # Implement the NOT gate logic
+    def perform_gate_logic(self, input_a):
+        # The NOT gate returns the opposite of the input (True becomes False, and vice versa)
+        return not input_a
