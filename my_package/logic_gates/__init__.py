01ff42e6a945e719292f36f3ebfeec98a63e858e
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 22:19:27 -0500

    Update __init__.py

--- my_package/logic_gates/__init__.py
+++ my_package/logic_gates/__init__.py
@@ -1,1 +1,6 @@
-
+from .and_gate import ANDGate
+from .or_gate import ORGate
+from .not_gate import NOTGate
+from .nand_gate import NANDGate
+from .nor_gate import NORGate
+from .xor_gate import XORGate
