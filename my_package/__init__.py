b80121f310321adff39cc7874d54c0475c5eb849
snasar1 <119819874+snasar1@users.noreply.github.com>
2023-11-08 19:24:12 -0500

    Create __init__.py

---
+++ my_package/__init__.py
@@ -0,0 +1,5 @@
+"""
+my_package: A Python package for implementing logic gates and connectors.
+
+This package contains classes and modules to create and connect logic gates for various logic operations.
+"""
